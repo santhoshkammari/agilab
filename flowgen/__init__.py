e80ca9cd455f1d0db9113b0e6e4c2c5a1f9c4602
santhoshkammari <santhoshkammari1999@gmail.com>
2025-08-09 19:08:30 +0530

    fix logs imports

--- flowgen/__init__.py
+++ flowgen/__init__.py
@@ -2,12 +2,16 @@ from .llm import BaseLLM, vLLM, Ollama
 from .llm.gemini import Gemini
 from .agent import Agent, AgentChain, create_agent
 from .template import BaseTemplate
+from .logger import UniversalLogger, get_logger
 
-__all__ = ['BaseLLM',
-           'vLLM',
-           'Ollama', 'Gemini',
-           'Agent',
-           'AgentChain',
-           'create_agent',
-           'BaseTemplate'
-           ]
+__all__ = [
+    'UniversalLogger',
+    "get_logger",
+    'BaseLLM',
+    'vLLM',
+    'Ollama', 'Gemini',
+    'Agent',
+    'AgentChain',
+    'create_agent',
+    'BaseTemplate'
+]
