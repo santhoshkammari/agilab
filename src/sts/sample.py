aa463f8e81c28a6a2d04e1d79beda1a1b38f1194
santhoshkammari <santhoshkammari199@gmail.com>
2025-09-01 20:02:59 +0530

    refactor

--- src/sts/sample.py
+++
@@ -1,8 +0,0 @@
-from RealtimeSTT import AudioToTextRecorder
-
-if __name__ == '__main__':
-    recorder = AudioToTextRecorder()
-    recorder.start()
-    input("Press Enter to stop recording...")
-    recorder.stop()
-    print("Transcription: ", recorder.text())
