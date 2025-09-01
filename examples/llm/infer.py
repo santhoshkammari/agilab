acd87cb42e0e4daecee8f7803fdb3ed9bdab7803
santhoshkammari <santhoshkammari199@gmail.com>
2025-09-01 20:08:30 +0530

    update

--- examples/llm/infer.py
+++ examples/llm/infer.py
@@ -1,23 +1,7 @@
-import asyncio
+from src.llm import LLM
 
-from flowgen import Gemini, Agent, Ollama
-from flowgen.tools.web import tool_functions as wt
-from flowgen.tools.markdown import tool_functions as mt
-from flowgen.tools.text_editor import tool_functions as tt
-from flowgen.tools.content_extract import tool_functions as ct
+llm = LLM('http://192.168.170.76:8000')
 
-llm = Gemini()
-# llm = Ollama(host='192.168.170.76',model="qwen3:1.7b")
+response = llm('hai')
 
-agent = Agent(llm,tools=[*list(mt.values()),*list(wt.values()),*list(ct.values())])
-# agent("do websearch for latest ai news, after that do search for latest modi news, and then latest ind vs eng match",temperature=0.0)
-# agent("provide python code part in /home/ntlpt59/master/own/flowgen/test/transformers.md")
-# agent("search for ai agents and extract information and summarize it fastly")
-#agent("search for Training and Finetuning Embedding Models with Sentence Transformers v3 , and get the loss function exact code, do until you findout code.")
-# agent("grpo trainer code example from official documentation transformers, get there exact code ",temperature=0.0)
-agent("Search for virat icc stats in wikipedia >>"
-      "get the content from wiki url >>"
-      "now find his stats table >> "
-      "summarize")
-# agent("virat vs rohit icc stats compare, first get virats precise information from wikipedia , then rohit and finally compare")
-# agent("find batting avg, runs scored,top scores in test for rohith sharam, do wiki >> tables  >> answer, go through each table till you find answer")
+print(response)
