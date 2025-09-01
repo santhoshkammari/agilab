b58dfab31e2624d130c2a9ed482c6fcd4225d748
santhoshkammari <santhoshkammari199@gmail.com>
2025-09-01 20:19:09 +0530

    add examples

--- examples/llm/infer.py
+++ examples/llm/infer.py
@@ -3,5 +3,13 @@ from src.llm import LLM
 llm = LLM('http://192.168.170.76:8000')
 
 response = llm('hai')
+print(response)
 
+
+from pydantic import BaseModel
+class Person(BaseModel):
+    name:str
+    age:int
+
+response = llm('hi',format=Person)
 print(response)
